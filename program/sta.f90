4e810926a45f75d38c14219f00a3d59e70513006
alloper_portatil <alloper@upv.edu.es>
2024-04-03 12:01:49 +0200

    debugging

--- program/sta.f90
+++ program/sta.f90
@@ -367,7 +367,7 @@ subroutine compute_turb_budget(c3,c4,p3,p4)
 
 call tra_coll2phys1d(c1,vel_r) !durdt
 !write(*,*) 'Memoria c3',c3%Re(end,end)
-write(*,*) 'Memoria de p3',ve_r%Re(i_pZ-1, i_Th-1, i_pN)
+write(*,*) 'Memoria de p3',vel_r%Re(i_pZ-1, i_Th-1, i_pN)
 !call tra_coll2phys1d(c4,p4) !durdz
 
 !do n = 1, mes_D%pN
