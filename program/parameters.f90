93517eb13ff82f4343c609475cbfbd39a9f1a43a
Alvarolaptop <alloper@upv.edu.es>
2024-04-04 12:24:25 +0200

    debugging

--- program/parameters.f90
+++ program/parameters.f90
@@ -45,9 +45,9 @@
    double precision, parameter :: d_minE3d      = 1d-5
    
 !-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-   integer,          parameter :: i_save_rate1  = 100   
+   integer,          parameter :: i_save_rate1  = 20   
    integer,          parameter :: i_save_rate2  = 10
-   integer,          parameter :: i_maxtstep    = 101 
+   integer,          parameter :: i_maxtstep    = 21 
    integer,          parameter :: s_step        = 5 ! interval to take statistics. 
    integer,          parameter :: n_sta         = i_save_rate1/s_step ! número de estadísticas 
    integer,          parameter :: f_step        = 1d8 ! Steps
