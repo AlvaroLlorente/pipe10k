96297a6a323cbe800dbb3340bf4996fe815446f1
alloper <alloper@upv.edu.es>
2024-05-12 17:14:53 +0200

    test

--- program/mpi.f90
+++ program/mpi.f90
@@ -11,7 +11,7 @@
 #ifdef _MPI
    include 'mpif.h'
    integer :: mpi_er, mpi_tg, mpi_rq(0:2*_Np), mpi_st(mpi_status_size)
-   integer :: new_comm
+   integer :: new_comm,subset_rank
    
 #endif
    integer :: mpi_rnk, mpi_sze, mpi_rnk_2,subset_size, i
@@ -34,7 +34,10 @@
       call mpi_comm_size(mpi_comm_world, mpi_sze, mpi_er)
       if(mpi_sze /= _Np) stop 'mpi_precompute: incorrect num procs'
 
+
       call MPI_COMM_RANK(MPI_COMM_WORLD, mpi_rnk_2, mpi_er)
+      call MPI_GROUP_INCL(MPI_COMM_WORLD, 15, subset_group, new_comm, mpi_er)
+      call MPI_COMM_CREATE(MPI_COMM_WORLD, new_comm, subset_rank, mpi_er)
       call MPI_COMM_SIZE(new_comm, subset_size, mpi_er)
 
 #endif
